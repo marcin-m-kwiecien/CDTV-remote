f89329d39dc38c416411c99e72441de1a493512e
sz33psz <sz33psz@gmail.com>
2022-10-26 14:30:54 +0200

    Initial commit

---
+++ app/src/main/java/xyz/mordeaux/cdtvremote/CDTVCode.java
@@ -0,0 +1,66 @@
+package xyz.mordeaux.cdtvremote;
+
+import com.google.common.primitives.Ints;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public enum CDTVCode {
+    MOUSE_A(0x080),
+    MOUSE_B(0x040),
+    MOUSE_UP(0x020),
+    MOUSE_DOWN(0x010),
+    MOUSE_LEFT(0x008),
+    MOUSE_RIGHT(0x004),
+    BUTTON_1(0x001),
+    BUTTON_2(0x021),
+    BUTTON_3(0x011),
+    BUTTON_4(0x009),
+    BUTTON_5(0x029),
+    BUTTON_6(0x019),
+    BUTTON_7(0x005),
+    BUTTON_8(0x025),
+    BUTTON_9(0x015),
+    BUTTON_0(0x039),
+    BUTTON_ESCAPE(0x031),
+    BUTTON_ENTER(0x035);
+
+    private static final Integer INITIAL_PULSE = 9000;
+    private static final Integer INITIAL_PAUSE = 4500;
+    private static final int REPEAT_PAUSE = 2100;
+
+    private static final Integer PULSE_SHORT = 400;
+    private static final Integer PULSE_LONG = 1200;
+    public static final int CODE_LENGTH = 12;
+
+    private final int code;
+
+    CDTVCode(int code) {
+        this.code = code;
+    }
+
+    public int[] pattern() {
+        List<Integer> pattern = new ArrayList<>();
+        pattern.add(INITIAL_PULSE);
+        pattern.add(INITIAL_PAUSE);
+        for (int position = CODE_LENGTH - 1; position >= 0; position--) {
+            pattern.add(PULSE_SHORT);
+            pattern.add(this.bitAt(position) ? PULSE_LONG : PULSE_SHORT);
+        }
+        for (int position = CODE_LENGTH - 1; position >= 0; position--) {
+            pattern.add(PULSE_SHORT);
+            pattern.add(this.bitAt(position) ? PULSE_SHORT : PULSE_LONG); //Reversed
+        }
+        pattern.add(PULSE_SHORT);
+        return Ints.toArray(pattern);
+    }
+
+    public static int[] repeatPattern() {
+        return new int[] {INITIAL_PULSE, REPEAT_PAUSE, PULSE_SHORT};
+    }
+
+    private boolean bitAt(int position) {
+        return (this.code & (1 << position)) != 0;
+    }
+}
---
+++ app/src/main/java/xyz/mordeaux/cdtvremote/MainActivity.java
@@ -0,0 +1,118 @@
+package xyz.mordeaux.cdtvremote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.ConsumerIrManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "irstuff";
+
+    private class ButtonsClickListener implements View.OnClickListener {
+        private final ConsumerIrManager consumerIrManager;
+
+        private ButtonsClickListener(ConsumerIrManager consumerIrManager) {
+            this.consumerIrManager = consumerIrManager;
+        }
+
+        @Override
+        public void onClick(View v) {
+            CDTVCode code = null;
+            switch (v.getId()) {
+                case R.id.button_mouse_up:
+                    code = CDTVCode.MOUSE_UP;
+                    break;
+                case R.id.button_mouse_down:
+                    code = CDTVCode.MOUSE_DOWN;
+                    break;
+                case R.id.button_mouse_left:
+                    code = CDTVCode.MOUSE_LEFT;
+                    break;
+                case R.id.button_mouse_right:
+                    code = CDTVCode.MOUSE_RIGHT;
+                    break;
+                case R.id.button_mouse_a:
+                    code = CDTVCode.MOUSE_A;
+                    break;
+                case R.id.button_mouse_b:
+                    code = CDTVCode.MOUSE_B;
+                    break;
+                case R.id.button_key_1:
+                    code = CDTVCode.BUTTON_1;
+                    break;
+                case R.id.button_key_2:
+                    code = CDTVCode.BUTTON_2;
+                    break;
+                case R.id.button_key_3:
+                    code = CDTVCode.BUTTON_3;
+                    break;
+                case R.id.button_key_4:
+                    code = CDTVCode.BUTTON_4;
+                    break;
+                case R.id.button_key_5:
+                    code = CDTVCode.BUTTON_5;
+                    break;
+                case R.id.button_key_6:
+                    code = CDTVCode.BUTTON_6;
+                    break;
+                case R.id.button_key_7:
+                    code = CDTVCode.BUTTON_7;
+                    break;
+                case R.id.button_key_8:
+                    code = CDTVCode.BUTTON_8;
+                    break;
+                case R.id.button_key_9:
+                    code = CDTVCode.BUTTON_9;
+                    break;
+                case R.id.button_key_0:
+                    code = CDTVCode.BUTTON_0;
+                    break;
+                case R.id.button_key_escape:
+                    code = CDTVCode.BUTTON_ESCAPE;
+                    break;
+                case R.id.button_key_enter:
+                    code = CDTVCode.BUTTON_ENTER;
+                    break;
+            }
+            if (code != null) {
+                consumerIrManager.transmit(40000, code.pattern());
+                Toast.makeText(MainActivity.this, "Sent code " + code.name(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButtonsClickListener listener = new ButtonsClickListener(
+                (ConsumerIrManager) getApplicationContext().getSystemService(Context.CONSUMER_IR_SERVICE));
+
+
+        findViewById(R.id.button_mouse_up).setOnClickListener(listener);
+        findViewById(R.id.button_mouse_down).setOnClickListener(listener);
+        findViewById(R.id.button_mouse_left).setOnClickListener(listener);
+        findViewById(R.id.button_mouse_right).setOnClickListener(listener);
+        findViewById(R.id.button_mouse_a).setOnClickListener(listener);
+        findViewById(R.id.button_mouse_b).setOnClickListener(listener);
+        findViewById(R.id.button_key_1).setOnClickListener(listener);
+        findViewById(R.id.button_key_2).setOnClickListener(listener);
+        findViewById(R.id.button_key_3).setOnClickListener(listener);
+        findViewById(R.id.button_key_4).setOnClickListener(listener);
+        findViewById(R.id.button_key_5).setOnClickListener(listener);
+        findViewById(R.id.button_key_6).setOnClickListener(listener);
+        findViewById(R.id.button_key_7).setOnClickListener(listener);
+        findViewById(R.id.button_key_8).setOnClickListener(listener);
+        findViewById(R.id.button_key_9).setOnClickListener(listener);
+        findViewById(R.id.button_key_0).setOnClickListener(listener);
+        findViewById(R.id.button_key_escape).setOnClickListener(listener);
+        findViewById(R.id.button_key_enter).setOnClickListener(listener);
+
+    }
+}
